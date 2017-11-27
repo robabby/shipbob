642a276806ec119fb92adb991ec9f85ed73c21d4
robabby <robabby23@gmail.com>
2017-11-27 10:01:40 -0600

    Cleanup

--- client/app/serializers/order.js
+++ client/app/serializers/order.js
@@ -9,9 +9,9 @@ export default ApplicationSerializer.extend(DS.EmbeddedRecordsMixin, {
     Ember.Logger.info('normalize:order/modelClass/', modelClass);
     Ember.Logger.info('normalize:order/resourceHash/', resourceHash);
     var data = {
-      id:            resourceHash.orderId,
-      type:          modelClass.modelName,
-      attributes:    resourceHash
+      id: resourceHash.orderId,
+      type: modelClass.modelName,
+      attributes: resourceHash
     };
     Ember.Logger.info('normalize:order/data/', { data });
     return { data: data };
